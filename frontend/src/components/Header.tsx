7c81f8e8eef57b5e4469c84796da1d131738a248
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-04-18 12:26:52 +0200

    Bygger på navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,18 @@
 function Header() {
-    return (
-        <header className="bg-white h-28 flex flex-col justify-end">
-            <nav className="flex items-center justify-between p-4">
-                <h1 className="text-xl font-bold">TravelBuddy</h1>
-                <button className="text-red-500">X</button>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="bg-background h-28 flex flex-col justify-end">
+      <nav className="flex items-center justify-between p-4">
+        <div className="flex">
+          <img src="./public/logo.svg" alt="" />
+          <h1 className="text-xl text-primary font-light">TravelBuddy</h1>
+        </div>
+        <div className="flex gap-4">
+            <img src="./public/account_circle.svg" alt="" />
+            <img src="./public/menu.svg" alt="" />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
