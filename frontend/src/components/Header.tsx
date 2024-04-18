ff31dfa1c25976835ccbf31ce359b0413ccf455f
dens0n <156082076+dens0n@users.noreply.github.com>
2024-04-18 15:46:35 +0200

    .

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,20 @@
 function Header() {
-  return (
-    <header className="bg-background h-28 flex flex-col justify-end">
-      <nav className="flex items-center justify-between p-4">
-        <div className="flex">
-          <img src="./public/logo.svg" alt="" />
-          <h1 className="text-xl text-primary font-light">TravelBuddy</h1>
-        </div>
-        <div className="flex gap-4">
-            <img src="./public/account_circle.svg" alt="" />
-            <img src="./public/menu.svg" alt="" />
-        </div>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="bg-background h-28 flex flex-col justify-end">
+            <nav className="flex items-center justify-between p-4">
+                <div className="flex">
+                    <img src="/logo.svg" alt="" />
+                    <h1 className="text-xl text-primary font-light">
+                        TravelBuddy
+                    </h1>
+                </div>
+                <div className="flex gap-4">
+                    <img src="/account_circle.svg" alt="" />
+                    <img src="/menu.svg" alt="" />
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
