a06deb2f4a9b163b30033b233e0d19b873967981
Yazukee <yasser.almaliki@live.se>
2024-04-26 18:42:25 +0200

    Rateing

---
+++ frontend/src/components/RateTheApp.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+
+function RateTheApp {
+  return (
+
+  )
+}
+
+export default RateTheApp
+
+
+// EJ inlagd i app.tsx eller i path finder, återstår att göra 
