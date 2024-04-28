7650e825204c8392f3133e9ef2b9a4488a95884a
Yazukee <yasser.almaliki@live.se>
2024-04-29 00:41:19 +0200

    Design

--- frontend/src/components/RateTheApp.tsx
+++ frontend/src/components/RateTheApp.tsx
@@ -1,47 +1,35 @@
-import { Link } from "react-router-dom"
+
 import { Button } from "./ui/button"
 
 function RateTheApp() {
   return (
-    <main>
-       <div>
-          <h1 className="text-center text-3xl text-secondary sm:text-6xl">Rate the app</h1>
+        <main>
+            <div className="flex justify-center h-screen flex-col items-center gap-7">
+              <h1 className="text-2xl text-primary md:text-4xl">Rate the app</h1>
+            
+                <h2 className="text-xl md:text-xl">Rate by pressing a star</h2>
 
-          <div>  
-            <p>Rate by pressing a star</p>
-              <div> </div>
-          </div>
-          
-              <div>
-                <p>Title</p>
-                <input type="text" />
-              </div>
-      
-              <div>
-                <p>Review</p>
-                <input type="text" />
-              </div>
+                  <div className="flex justify-start">
+                  <p>Title</p> 
+                  </div>
 
-                <div>
-                    <Button variant="outline" size="md">
-                      <p className="text-neutral-200">CANCEL</p>
-                        <img className="ml-2 h-6 w-6" src="" alt=""/>
-                    </Button>
 
-                    <img src="./icons/vector-icon.svg" alt="Vectoricon"/>
-                
-                    <Button variant="default">
-                      <p>SEND</p>
-                        <img className="ml-2 h-6 w-6" src="" alt=""/>
-                    </Button>
+                  <img src="./icons/Vector2525.svg" alt="" />
+                  <div>
+                    <p>Review</p>
+                  </div>
 
-              </div>
-        </div>
-    </main>
+                    <div className="flex justify-between gap-5">
+                        <Button className= "border border-input text-primary border-primary bg-background w-40">CANCEL</Button>
+                        <img src="./icons/vector-icon.svg" alt="Vectoricon"/>
+                        <Button className=" flex w-40" >SEND</Button>
+                  </div>
+            </div>
+       </main>
   )
 }
 
 export default RateTheApp
 
 
-// EJ inlagd i app.tsx eller i path finder, återstår att göra 
+// Stjärnan ej lagt till heter icon-ratingstar.svg
