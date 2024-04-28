356ecd1db27be06c86fc461e9bd2c4fb60c6d080
Yazukee <yasser.almaliki@live.se>
2024-04-28 22:55:38 +0200

    svg plus styling

--- frontend/src/components/RateTheApp.tsx
+++ frontend/src/components/RateTheApp.tsx
@@ -4,26 +4,39 @@ import { Button } from "./ui/button"
 function RateTheApp() {
   return (
     <main>
-    <div>
-      <h1 className="text-center text-3xl text-secondary sm:text-6xl">Rate the app</h1>
-      <p>Rate by pressing a star</p>
+       <div>
+          <h1 className="text-center text-3xl text-secondary sm:text-6xl">Rate the app</h1>
 
-      <img src="" alt="" />
+          <div>  
+            <p>Rate by pressing a star</p>
+              <div> </div>
+          </div>
+          
+              <div>
+                <p>Title</p>
+                <input type="text" />
+              </div>
+      
+              <div>
+                <p>Review</p>
+                <input type="text" />
+              </div>
 
-      <div>
-        <p>Title</p>
-        <input type="text" />
-      </div>
-    <div>
-      <p>Review</p>
-      <input type="text" />
-    </div>
+                <div>
+                    <Button variant="outline" size="md">
+                      <p className="text-neutral-200">CANCEL</p>
+                        <img className="ml-2 h-6 w-6" src="" alt=""/>
+                    </Button>
 
-      <div>
-        <Button>CANSEL</Button>
-        <Button>SEND</Button>
-      </div>
-    </div>
+                    <img src="./icons/vector-icon.svg" alt="Vectoricon"/>
+                
+                    <Button variant="default">
+                      <p>SEND</p>
+                        <img className="ml-2 h-6 w-6" src="" alt=""/>
+                    </Button>
+
+              </div>
+        </div>
     </main>
   )
 }
