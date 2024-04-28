3fa1dcb1248b1796e717e870f1b1e472582d5519
Yazukee <yasser.almaliki@live.se>
2024-04-28 22:16:06 +0200

    Styling

--- frontend/src/components/RateTheApp.tsx
+++ frontend/src/components/RateTheApp.tsx
@@ -1,9 +1,30 @@
 import { Link } from "react-router-dom"
 import { Button } from "./ui/button"
 
-function RateTheApp {
+function RateTheApp() {
   return (
+    <main>
+    <div>
+      <h1 className="text-center text-3xl text-secondary sm:text-6xl">Rate the app</h1>
+      <p>Rate by pressing a star</p>
 
+      <img src="" alt="" />
+
+      <div>
+        <p>Title</p>
+        <input type="text" />
+      </div>
+    <div>
+      <p>Review</p>
+      <input type="text" />
+    </div>
+
+      <div>
+        <Button>CANSEL</Button>
+        <Button>SEND</Button>
+      </div>
+    </div>
+    </main>
   )
 }
 
