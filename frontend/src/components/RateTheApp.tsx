c5bde96671a5e5777a24db4815f1bf3e0f439e0e
Yazukee <yasser.almaliki@live.se>
2024-04-30 00:12:56 +0200

    Rate the app

--- frontend/src/components/RateTheApp.tsx
+++ frontend/src/components/RateTheApp.tsx
@@ -3,28 +3,16 @@ import { Button } from "./ui/button"
 
 function RateTheApp() {
   return (
-        <main>
-            <div className="flex justify-center h-screen flex-col items-center gap-7">
-              <h1 className="text-2xl text-primary md:text-4xl">Rate the app</h1>
+        <main className="flex flex-col  h-screen items-center pt-32 pr-4 pl-4 pb-4">
+          <div className="flex flex-col justify- items-center gap-4">
+            <h1 className="text-3xl font-bold text-secondary">Rate The app</h1>
+            <h1 className="text-xl md:text-2xl">Rate by pressing a star</h1>
+          </div>
+
+          <div>
             
-                <h2 className="text-xl md:text-xl">Rate by pressing a star</h2>
+          </div>
 
-                  <div className="flex justify-start">
-                  <p>Title</p> 
-                  </div>
-
-
-                  <img src="./icons/Vector2525.svg" alt="" />
-                  <div>
-                    <p>Review</p>
-                  </div>
-
-                    <div className="flex justify-between gap-5">
-                        <Button className= "border border-input text-primary border-primary bg-background w-40">CANCEL</Button>
-                        <img src="./icons/vector-icon.svg" alt="Vectoricon"/>
-                        <Button className=" flex w-40" >SEND</Button>
-                  </div>
-            </div>
        </main>
   )
 }
