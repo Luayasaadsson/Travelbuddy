e590b5d85ed19e467f5068431bafd0110b3d54a0
dens0n <156082076+dens0n@users.noreply.github.com>
2024-05-08 13:41:57 +0200

    kommenterat ut footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -64,7 +64,7 @@ function App() {
                     <Route path="/aboutus" element={<AboutUs />} />
                 </Routes>
             </BrowserRouter>
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
