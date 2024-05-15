4e777642c8dd767670fb3534192afdc4ccb02244
Damian Amalraj. <damian.amalraj@hotmail.com>
2024-05-15 12:03:13 +0200

    Add Dockerfile for containerization and configured Program.cs for production environment

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -78,11 +78,9 @@ var app = builder.Build();
 app.MapIdentityApi<User>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors("CorsPolicy");
