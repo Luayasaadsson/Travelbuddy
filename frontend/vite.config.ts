f2c5e8f1eff09ac11ba4a38697b9ff5d3138cb05
dens0n <156082076+dens0n@users.noreply.github.com>
2024-05-21 19:29:48 +0200

    ändrat till https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import fs from "fs"
 
 export default defineConfig({
     plugins: [react()],
@@ -9,4 +10,11 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    server: {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+        },
+        port: 5173,
+      },
 });
