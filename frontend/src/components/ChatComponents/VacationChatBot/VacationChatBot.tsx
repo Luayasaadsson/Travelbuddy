a402d55ae8cbfd9f834b118d6c6d369f9b29cf9c
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-06-02 01:15:51 +0200

    Uppdaterade VacationChatBot

--- frontend/src/components/ChatComponents/VacationChatBot/VacationChatBot.tsx
+++ frontend/src/components/ChatComponents/VacationChatBot/VacationChatBot.tsx
@@ -34,7 +34,7 @@ export default function VacationChatBot() {
         let message
 
         if (isDestinationQuery) {
-            message = `I'm interested in traveling for ${buttonChoice}`
+            message = `I'm interested in traveling to ${buttonChoice}`
             dispatch(
                 updateMessageList({
                     type: "text",
@@ -46,7 +46,7 @@ export default function VacationChatBot() {
                 updateMessageList({
                     type: "text",
                     role: "agent",
-                    content: `Great choice! Here are some things to keep in mind regarding this vacation: <br><strong>Attractions, Weather, Travel Tips</strong>`,
+                    content: "Okay! What specific attractions or activities are you looking for?",
                 }),
             )
         } else {
@@ -80,25 +80,51 @@ export default function VacationChatBot() {
         const destinationKeywords = vacationList.map((vacation) =>
             vacation.label.toLowerCase(),
         )
-        let isDestinationQuery = destinationKeywords.some((keyword) =>
-            query.toLowerCase().includes(keyword),
-        )
+        const vacationKeywordsPhrases = ["want", "I want to explore", "explore", "for", "like"]
+
+        let isDestinationQuery = false
+        let isKeywordQuery = false
+
+        for (const keyword of destinationKeywords) {
+            if (query.toLowerCase().includes(keyword.toLowerCase())) {
+                isDestinationQuery = true
+                break
+            }
+        }
+
+        for (const keyword of vacationKeywordsPhrases) {
+            if (query.toLowerCase().includes(keyword.toLowerCase())) {
+                isKeywordQuery = true
+                break
+            }
+        }
 
         if (isDestinationQuery) {
-            return `User Query: "I am located in ${location} and interested in visiting ${query}"\n\nResponse: Provide details of <strong>top attractions</strong>, <strong>recommended travel period</strong>, and <strong>essential tips for visitors</strong>. Include no other text.`
+            console.log("väljer prompt 1")
+
+            return `User Query: "I am located in ${location} and interested in visiting ${query}"\n\nResponse: Respond only with a list containing exactly five top attractions. Write the attractions in a row, separated by commas. Include no other text.`
+        } else if (isKeywordQuery) {
+            console.log("väljer prompt 2")
+
+            return `${context}\n\nUser Query: "I am located in ${location} and ${query}"\n\nResponse: Provide details of exactly six vacation destinations at my location. For each destination, include the following format: \n\n**Name of the Destination**\nA short description\n**Google maps:** <a href="https://www.google.com/maps/search/name+of+the+destination+mylocation/" target="_blank">Name</a>\n**Visit website:** <a href="The real URL to the destination's website that's linked to the destination in google maps" target="_blank">Name</a>\n\nInclude no additional text.`
         } else {
-            return `${context}\n\nUser Query: "I want to travel from ${location} to ${query}, any advice?"\n\nResponse: Provide travel advice including best travel routes, recommended airlines, and any travel warnings. Include no additional text.`
+            console.log("väljer prompt 3")
+
+            return `${context}\n\nUser Query: "${query}"\n\nResponse: You are a helpful assistant. Answer the user's question in a friendly and informative manner.`
         }
     }
 
     const fetchAgentResponse = async (query: string) => {
         setIsLoading(true)
+
         try {
             const context = messageList
                 .map((message) => `${message.role}: ${message.content}`)
                 .join("\n")
             const prompt = generatePrompt(context, query)
-            const response = await axios.post(
+            console.log(prompt)
+
+            const fetchPromise = axios.post(
                 "https://localhost:7038/api/OpenAi/AskAiAssistant",
                 {
                     question: prompt,
@@ -113,6 +139,7 @@ export default function VacationChatBot() {
                     withCredentials: true,
                 },
             )
+            const [response] = await Promise.all([fetchPromise])
 
             if (response.status !== 200) {
                 throw new Error("Network problem")
@@ -128,13 +155,26 @@ export default function VacationChatBot() {
     }
 
     const filterAgentResponse = (message: string) => {
-        dispatch(
-            updateMessageList({
-                type: "text",
-                role: "agent",
-                content: message,
-            }),
-        )
+        // Split message into words
+        const words = message.split(",").map((word) => word.trim())
+
+        if (words.length === 10) {
+            dispatch(
+                updateMessageList({
+                    type: "button",
+                    role: "agent",
+                    content: message,
+                }),
+            )
+        } else {
+            dispatch(
+                updateMessageList({
+                    type: "text",
+                    role: "agent",
+                    content: message,
+                }),
+            )
+        }
     }
 
     return (
